c20a9bfd5a37b1911e68fb86964f4f364b9993f5
Hidetake Iwata <int128@gmail.com>
2019-03-06 10:59:26 +0900

    Release to Homebrew tap repository

---
+++ .circleci/homebrew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -xe
+
+test "$CIRCLE_TAG"
+
+dist_sha256=$(shasum -a 256 -b dist/ghcp_darwin_amd64 | cut -f1 -d' ')
+
+cat <<EOF
+class Ghcp < Formula
+  desc "Commit and Push files to GitHub repository"
+  homepage "https://github.com/int128/ghcp"
+  url "https://github.com/int128/ghcp/releases/download/${CIRCLE_TAG}/ghcp_darwin_amd64"
+  version "${CIRCLE_TAG}"
+  sha256 "${dist_sha256}"
+
+  def install
+    bin.install "ghcp_darwin_amd64" => "ghcp"
+  end
+
+  test do
+    system "#{bin}/ghcp -h"
+  end
+end
+EOF
