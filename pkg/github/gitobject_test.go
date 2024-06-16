5aebbc1d92fb055dfbd37c15790ff3458ddf609a
Hidetake Iwata <int128@gmail.com>
2024-06-16 12:06:43 +0900

    Fix test

--- pkg/github/gitobject_test.go
+++ pkg/github/gitobject_test.go
@@ -25,7 +25,7 @@ func TestGitHub_CreateCommit(t *testing.T) {
 				Message: github.String("message"),
 				Parents: []*github.Commit{{SHA: github.String("parentCommitSHA")}},
 				Tree:    &github.Tree{SHA: github.String("treeSHA")},
-			}).
+			}, nil).
 			Return(&github.Commit{
 				SHA: github.String("commitSHA"),
 			}, nil, nil)
@@ -56,7 +56,7 @@ func TestGitHub_CreateCommit(t *testing.T) {
 			CreateCommit(ctx, "owner", "repo", &github.Commit{
 				Message: github.String("message"),
 				Tree:    &github.Tree{SHA: github.String("treeSHA")},
-			}).
+			}, nil).
 			Return(&github.Commit{
 				SHA: github.String("commitSHA"),
 			}, nil, nil)
